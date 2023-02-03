4b4758b38ced3f3c4ae424e312fadb7ecec62ca8
TRA_Scorer <luis@zyp.mx>
2023-02-02 19:51:29 -0600

    favicon plus corrections to css and html title

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel</title>
+    <title>¿Quién Será?</title>
 
     <!-- Scripts -->
     <script src="/js/jquery-3.1.1.min.js" defer></script>
@@ -20,6 +20,17 @@
     <link href="/css/font-awesome.css" rel="stylesheet">
     <link href="/css/main.css?v=1.2.1" rel="stylesheet">
     <link href="/css/responsive.css?v=1.2.1" rel="stylesheet">
+
+    <!-- Favicon -->
+    <link rel="apple-touch-icon" sizes="144x144" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
+    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#ff4400">
+    <meta name="msapplication-TileColor" content="#ff4400">
+    <meta name="theme-color" content="#000000">
+
+
 
 </head>
 <body>
